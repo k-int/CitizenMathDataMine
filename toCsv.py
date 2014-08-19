f286129410279a487b8786a400d9f99f04c1fd61
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-08-19 18:15:24 +0100

    updated toCsv

--- toCsv.py
+++ toCsv.py
@@ -22,5 +22,27 @@ student_properties_data_as_json = json.loads(student_property_data)
 
 print "Got json"
 
-pprint(student_data_as_json)
-pprint(student_properties_data_as_json)
+# pprint(student_data_as_json)
+# pprint(student_properties_data_as_json)
+
+student_data = {}
+
+for student in student_data_as_json['rows']:
+  # print "Processing student "+student['key.name']+' '+student['user_id']
+  # print "Additional fields "+student['additional_fields']
+  additional_data = json.loads(student['additional_fields'])
+  student_data[student['user_id']] = { 'name':student['key.name'], 'additional':additional_data }
+
+for student_property in student_properties_data_as_json['rows']:
+  student_id_value = student_property['key.name']
+  student_id_components = student_id_value.split('-');
+  student_id = student_id_components[0]
+  print student_id
+  student_info_map = student_data[student_id]
+  if student_info_map :
+    print('Located that student')
+  else:
+    print('Cant find that student')
+
+
+
