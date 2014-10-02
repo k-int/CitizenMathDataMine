dcf705b0146842c3bbea42ddc158cd2897dc202b
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-02 23:18:00 +0100

    updated

--- learnerProgressCsv.py
+++ learnerProgressCsv.py
@@ -59,7 +59,11 @@ for student_property in student_properties_data_as_json['rows']:
   student_id_components = student_id_value.split('-');
   student_id = student_id_components[0]
   json_str = student_property['value']
+  last_updated_str = student_property['updated_on']
+  if last_updated_str is None:
+    last_updated_str = ''
   json_value = None
+
   if ( json_str is not None ) :
     json_value = json.loads(json_str);
   # print student_id
@@ -70,6 +74,7 @@ for student_property in student_properties_data_as_json['rows']:
     # print('Located that student')
 
     student_info_map['value'] = json_value
+    student_info_map['updated_on'] = last_updated_str
     if ( json_value != None ) :
       for key in json_value :
         if ( key not in completion_status ) :
@@ -88,54 +93,54 @@ for student_property in student_properties_data_as_json['rows']:
 learner_types.sort()
 additional_properties.sort()
 
-print( 'name', end='' )
-for prop in additional_properties :
-  print(',' + prop, end ='' )
-
-for lt in learner_types:
-  print(',' + lt, end ='' )
-
-# for prop in completion_status :
-#   print(',' + prop, end='' )
+# print( 'name', end='' )
+# for prop in additional_properties :
+#   print(',' + prop, end ='' )
 
 
+print('UserId,FamilyName,GivenName,LastUpdated', end='' )
+
+for prop in completion_status :
+  print(',' + prop, end='' )
 
 print('')
 
 for student_key in student_data :
   student = student_data[student_key]
   print("\""+student['name']+"\",", end='')
-  for prop in additional_properties :
-    print("\"",end='')
-    if ( prop in student['additional'] ) :
-      ctr = 0
-      for v in student['additional'][prop]:
-        if ( ctr > 0 ) :
-          print(' ',end='')
-        print(v,end='')
-        ctr = ctr +1
-    else :
-      print('', end='')
-    print("\",",end='')
 
-  # for completion_key in completion_status:
-  #   if ( completion_key in student['value'] ) :
-  #     print(",\"", end="");
-  #     print(student['value'][completion_key], end="")
-  #     print("\"", end="");
+  if ( 'FamilyName' in student['additional'] ) :
+    print("\""+(student['additional']['FamilyName'][0])+"\",", end='')
+  else :
+    print("\"\",", end='')
+
+  if ( 'GivenName' in student['additional'] ) :
+    print("\""+(student['additional']['GivenName'][0])+"\",", end='')
+  else :
+    print("\"\",", end='')
+
+  print("\""+student['updated_on']+"\"", end='')
+
+  # for prop in additional_properties :
+  #   print("\"",end='')
+  #   if ( prop in student['additional'] ) :
+  #     ctr = 0
+  #     for v in student['additional'][prop]:
+  #       if ( ctr > 0 ) :
+  #         print(' ',end='')
+  #       print(v,end='')
+  #       ctr = ctr +1
   #   else :
-  #     print(',', end='')
+  #     print('', end='')
+  #   print("\",",end='')
 
-  # LEarner type processing
-  for lt in learner_types:
-    # If the student has a learner type property
-    print("\"",end='')
-    if 'learner_type' in student['additional'].keys() and lt in student['additional']['learner_type']:
-      print('Y',end='')
-    else:
-      print('N',end='')
-    print("\",",end='')
-    
+  for completion_key in completion_status:
+    if ( completion_key in student['value'] ) :
+      print(",\"", end="");
+      print(student['value'][completion_key], end="")
+      print("\"", end="");
+    else :
+      print(',', end='')
+
   print('')
-
 
---
+++ runall.sh
@@ -0,0 +1,4 @@
+python toCsv.py  > ~/Desktop/learners`date "+%y_%m_%d_%h_%m"`.csv
+python learnerProgressCsv.py  > ~/Desktop/learner_progression_`date "+%y_%m_%d_%h_%m"`.csv
+
+
